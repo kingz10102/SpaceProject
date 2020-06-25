91b76a1931deda213df9ef1b975729d1d5a71e76
Kingzz <krem@Kingzzs-MBP.fios-router.home>
2020-06-25 17:48:38 -0400

    This my second commit

---
+++ app.js
@@ -0,0 +1,37 @@
+/////////// first draft/////////////
+
+
+//Constructor function for players spaceship
+class Players_SpaceShip {
+    constructor() {
+        this.hitpoints = 20;
+        this.firepower = 5;
+        this.accuracy = 0.7;
+    }
+}
+
+    Attack(Player_Spaceship) 
+      if (Math.random() <= 0.7) {
+        return "Enemy target has been hit";
+      
+      } else {
+        return "Attack was missed";
+      }
+    }
+  }
+
+//   class Alien_Spaceships {
+//     constructor(hitpoints, firepower, accuracy) {
+//       this.hitpoints = hitpoints;
+//       this.firepower = firepower;
+//       this.accuracy = accuracy;
+//     }
+//     attack(Alien_Spaceships) {
+//       if (Math.random() <= this.accuracy) {
+//         return "We've been partially immoblized has been hit";
+//       } else {
+//         return "Thats right. They missed";
+//       }
+//     }
+//   }
+  
