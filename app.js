2f9dc9b9e479aed3cc53ff71725a7ed2a608078f
Kingzz <krem@Kingzzs-MBP.fios-router.home>
2020-06-27 12:40:34 -0400

    third draft commit

--- app.js
+++ app.js
@@ -1,37 +1,74 @@
-/////////// first draft/////////////
-
-
-//Constructor function for players spaceship
-class Players_SpaceShip {
-    constructor() {
-        this.hitpoints = 20;
-        this.firepower = 5;
-        this.accuracy = 0.7;
+let playerSpaceship = {
+    hull: 20,
+    firepower: 5,
+    accuracy: 0.7
+  };
+  
+class azanAliens {
+    constructor(hitPoints, firePower, accuracy) {
+        this.hull = hitPoints();
+        this.firepower = firePower(); //making a function call withing a constructor 
+        this.accuracy = accuracy();
     }
 }
+  
+  // attributes given for player is converted to a function
+  // used 
+  function hitPoints() {
+    let hullRange = [3, 4, 5, 6];
+    let hullStrength = hullRange[Math.floor(Math.random() * hullRange.length)];
+    return hullStrength;
+  }
+  function firePower() {
+    let firepowerRange = [2, 3, 4];
+    let firepowerStrength = firepowerRange[Math.floor(Math.random() * firepowerRange.length)];
+    return firepowerStrength;
+  }
+  function accuracy() {
+    let accuracyRange = [0.6, 0.7, 0.8];
+    let accuracyStrength = accuracyRange[Math.floor(Math.random() * accuracyRange.length)];
+    return accuracyStrength;
+  }
+  
+  
+// alien generator this whole loop would provide randomazation to which alien would approach ship to battle
+  
+  let alienRoulette = [];
+  function enemyOrder() {
+    for (let counter = 0; counter < 6; counter++) {
+      let alien = new Aliens();
+      alienArmy.push(alien);
+      console.log(alienRoulette);
+    }
+  }
+  console.log(playerSpaceship);
+  
 
-    Attack(Player_Spaceship) 
-      if (Math.random() <= 0.7) {
-        return "Enemy target has been hit";
-      
+  
+  function startGame() {
+    let name = prompt('What is your name?')
+    alert(
+      `Welcome to Space Battle: Galaxy Wars. We need your help Captian ${name}. The aliens of the Planet Azan have attacked Earth and we need your help. Defend our home!`
+    );
+    promptAction();
+  }
+  startGame();
+  
+  function boxAction() {
+    if (playerSpaceship.hull > 0) {
+      let playerCommand = prompt(
+        `The enemy is among us. What shall we do you want to do?`);
+      if (playerCommand === "attack" || playerCommand === "Attack" || playerCommand === "a" || playerCommand === "A") {
+        playerAttack();
+      } else if (playerCommand=== "retreat" || playerCommand === "Retreat" || playerCommand === "r" || playerCommand === "R") {
+        playerRetreat();
+        loseGame();
       } else {
-        return "Attack was missed";
+        alert(
+          `We need a command captain there is no time for waddling!!!!!`);
+        boxAction();
       }
     }
   }
-
-//   class Alien_Spaceships {
-//     constructor(hitpoints, firepower, accuracy) {
-//       this.hitpoints = hitpoints;
-//       this.firepower = firepower;
-//       this.accuracy = accuracy;
-//     }
-//     attack(Alien_Spaceships) {
-//       if (Math.random() <= this.accuracy) {
-//         return "We've been partially immoblized has been hit";
-//       } else {
-//         return "Thats right. They missed";
-//       }
-//     }
-//   }
+  
   
